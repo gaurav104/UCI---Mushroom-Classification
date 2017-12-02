cf76d8e1f94345893b9aa2d8b954e2e9c359e372
Gaurav Patel <gouravpatel8@gmail.com>
2017-12-03 00:45:22 +0530

    Update Mushroom.py

--- Mushroom.py
+++ Mushroom.py
@@ -1,12 +1,12 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
+#Data processing and feature selection
 dataset = pd.read_csv('mushrooms.csv')
 dataset = dataset.drop(['veil-type','veil-color','gill-attachment','stalk-root'],axis=1)
 X = dataset.iloc[:,1:]
 y = dataset.iloc[:,0].values
 
+#Data encoding
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X = X.apply(labelencoder_X.fit_transform) # encoding multiple columns
@@ -15,10 +15,11 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y,)
 
+#Model(Classifier)
 from sklearn.svm import SVC
 classifier = SVC(kernel='linear')
 
-
+#Cross-Validation
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X, y = y, cv = 10, n_jobs = -1)
 mean = accuracies.mean()
